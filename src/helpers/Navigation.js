4ee7810b1cd3bfc97b580fcd7426f3e7f7da7c83
Rowinbot <rowinkaleb@gmail.com>
2019-01-11 03:11:35 -0500

    Add withReplace helper that adds a method to navigators that let you replace the actual screen

--- src/helpers/Navigation.js
+++ src/helpers/Navigation.js
@@ -129,3 +129,25 @@ export const topTabNavigator = ({ initialScreenName }) => ({
     }
   }
 })
+
+const replaceScreen = (action, state) => {
+  const routes = state.routes.slice(0, state.routes.length - 1)
+  routes.push(action)
+  return {
+    ...state,
+    routes,
+    index: routes.length - 1
+  }
+}
+
+export const withReplace = Router => {
+  const prevGetStateForAction = Router.router.getStateForAction
+
+  Router.router.getStateForAction = (action, state) => {
+    if (state && action.type === 'ReplaceCurrentScreen')
+      return replaceScreen(action, state)
+    return prevGetStateForAction(action, state)
+  }
+
+  return Router
+}
